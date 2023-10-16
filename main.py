90f8e2b5ba6a820c5ed14cb2c2d5d108dd227186
SenasCodes <147446008+SenasCodes@users.noreply.github.com>
2023-10-16 18:25:36 +0100

    Understanding  thiznts

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-print("Hello pycharmm")
+import time
 
-a = 1
-b = 20
+#print("Hello pycharmm")
 
-print(a+b)
+#a = "ola"
+#b = "cara de cu"
+#c = 10
+
+#c=str(c)
+
+#print(a)
+
+#name= input("What is your name:")
+#altura = input("que altura tens em cm")
+
+#print (name + str(altura))
+
+#while 1==1:and  nome != "joao"
+#nome = input("qmq te chamas zé:")
+
+#while len(nome) != "" and  nome != "joao":
+    #print("tenta outra vez")
+# nome = input("qmq te chamas zé:")
+
+#print("assim memo bertolo")
+
+c = 10
+l = 10
+d = 1
+
+for i in range(c):
+    for j in range (l):
+        if j == 3:
+            break
+        d=d+1
+        print(str(d)+" ", end="")
+        time.sleep(0.3)
+    print()
+print("xupapi")
